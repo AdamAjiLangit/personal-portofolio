8e90c60cab71c32e19fc6a8e05e4e432ba3b52fb
Adam <105685257+AdamAjiLangit@users.noreply.github.com>
2024-08-18 18:15:02 +0700

    fix : [AD] intro

--- components/sub/intro.tsx
+++ components/sub/intro.tsx
@@ -62,7 +62,7 @@ export default function Intro() {
                 initial={{ opacity: 0, y: 100 }}
                 animate={{ opacity: 1, y: 0 }}
             >
-                <span className="font-bold">Hello, I&apos;m Adam.</span> I'm a{" "}
+                <span className="font-bold">Hello, I&apos;m Adam.</span> I&apos;m a{" "}
                 <span className="font-bold">web developer</span> with{" "}
                 <span className="font-bold">2 years</span> of experience. I enjoy
                 building <span className="italic">sites & apps</span>. My focus is{" "}
