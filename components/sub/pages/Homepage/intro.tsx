1852827150405b8cfe5c4b7ab9fb752454fa74e6
Adam <105685257+AdamAjiLangit@users.noreply.github.com>
2025-01-10 10:10:17 +0700

    [AD] cv-update

--- components/sub/pages/Homepage/intro.tsx
+++ components/sub/pages/Homepage/intro.tsx
@@ -92,7 +92,7 @@ export default function Intro() {
 
                 <a
                     className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-                    href="/newCV.docx"
+                    href="/myCV.pdf"
                     download
                 >
                     Download CV{" "}
